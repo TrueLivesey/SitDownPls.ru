22c37eb4a9c013fd0c53afa4a455be2cae58aaeb
Nikita Pavlov <nek-pavlov@yandex.ru>
2023-07-16 21:32:09 +0300

    added gulp v.1

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// подключение npm-пакета Swiper (слайдер)
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
